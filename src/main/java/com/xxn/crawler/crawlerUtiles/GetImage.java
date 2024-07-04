ee50c189ac335763f6e61cb81398f978cb7b232b
QinXingkang <2505083096@qq.com>
2024-07-04 17:21:31 +0800

    抓取图片03,返回ArrayList

--- src/main/java/com/xxn/crawler/crawlerUtiles/GetImage.java
+++ src/main/java/com/xxn/crawler/crawlerUtiles/GetImage.java
@@ -22,6 +22,7 @@ import java.awt.*;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,8 +37,9 @@ public class GetImage implements PageProcessor {
     private String url;
     private String path;
     private Spider spider;
-    public static String imageSiteUrl = "https://www.nipic.com/topic/show_27202_1.html";
+    private String imageSiteUrl;
 
+    private ArrayList<String> imagesUrl;
     public GetImage() {
     }
 
@@ -46,46 +48,53 @@ public class GetImage implements PageProcessor {
         this.path = path;
     }
 
+    public GetImage(String url, String path, String imageSiteUrl) {
+        this.url = url;
+        this.path = path;
+        this.imageSiteUrl = imageSiteUrl;
+    }
+
     @Override
     public void process(Page page) {
         Html html = page.getHtml();
         List<String> all = html.$("img","src").regex(".*https.*").all();
         page.putField("url", all);
 
-        downLoadImg(imageSiteUrl);
+        imagesUrl = (ArrayList<String>) all;
+
+//        downLoadImg(imageSiteUrl);
     }
 
-    public void downLoadImg(String imageSiteUrl){
-        try {
-            Document document = Jsoup.connect(imageSiteUrl).get();
-            Elements elements = document.select("li.new-search-works-item");
-            for (int i = 0; i < elements.size(); i++) {
-                Elements select = elements.get(i).select("a > img");
-                Connection.Response response = Jsoup.connect("https:" + select.attr("src")).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0").ignoreContentType(true).execute();
-                //添加ip代理版本Jsoup.connect(select.attr("src")).proxy().execute();
-                //模拟浏览器Jsoup.connect(select.attr("src")).userAgent().execute();
-                String name = select.attr("alt");
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(response.bodyAsBytes());
-//                Image image = new Image();
-
-                FileUtils.copyInputStreamToFile(byteArrayInputStream,new File("D://fileitem//" + name + ".png"));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public void downLoadImg(String imageSiteUrl){
+//        try {
+//            Document document = Jsoup.connect(imageSiteUrl).get();
+//            Elements elements = document.select("li.new-search-works-item");
+//            for (int i = 0; i < elements.size(); i++) {
+//                Elements select = elements.get(i).select("a > img");
+//                Connection.Response response = Jsoup.connect("https:" + select.attr("src")).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0").ignoreContentType(true).execute();
+//                //添加ip代理版本Jsoup.connect(select.attr("src")).proxy().execute();
+//                //模拟浏览器Jsoup.connect(select.attr("src")).userAgent().execute();
+//                String name = select.attr("alt");
+//                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(response.bodyAsBytes());
+//
+//                FileUtils.copyInputStreamToFile(byteArrayInputStream,new File("D://fileItem//"+ name + ".png"));
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     public Site getSite() {
         return Site.me();
     }
 
-    public String start() {
-        spider = Spider.create(new GetImage())
+    public ArrayList<String> start() {
+        spider = Spider.create(this)
                 .addUrl(url)
                 .addPipeline(new FilePipeline(path))
                 .thread(5);
         spider.start();
-        return null;
+        return imagesUrl;
     }
 }
--- src/test/java/com/xxn/crawler/Test.java
+++ src/test/java/com/xxn/crawler/Test.java
@@ -1,11 +1,13 @@
 package com.xxn.crawler;
 
+import com.xxn.crawler.crawlerUtiles.GetImage;
 import com.xxn.crawler.crawlerUtiles.GetNews;
 import com.xxn.crawler.pojo.News;
 
 public class Test {
     public static void main(String[] args) {
-        GetImage getImages = new GetImage("https://www.nipic.com/", "D:\\test\\pic");
+
+        GetImage getImages = new GetImage("https://www.nipic.com/", "D://test//pic","https://www.nipic.com/topic/show_27202_1.html");
         getImages.start();
 //        GetNews getNews = new GetNews("https://www.hhu.edu.cn/2024/0629/c166a285661/page.htm","D:\\test");
 //        News start = getNews.start();
